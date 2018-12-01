c7ebf492874518a2d134e228421cf9ca732391c6
Adrian Ciepły <adriancieply@MacBook-Pro-Adrian.local>
2018-12-01 09:48:30 +0100

    tablica articles

--- cms/js/articles.js
+++ cms/js/articles.js
@@ -1,46 +1,70 @@
+const getPostsUrl = 'http://localhost/Blog/api/controlers/posts/read.php';
+const getOnePostUrl = 'http://localhost/Blog/api/controlers/posts/read_one.php';
+const tabID = 'articlesTab';
+
+let getPosts = (refresh = false) => {
+    $.ajax({
+        url: getPostsUrl,
+        dataType: "JSON",
+        success: function (data) {
+            let posts = data.data;
+            postsTable(posts, refresh);
+        },
+        error: function (request, status, error) {
+            console.log(error);
+        }
+    })
+}
+
+let postsTable = (data, refresh=false) => {
+    let fTab = $("#" + tabID);
+    let db;
+    if(!refresh) {
+        db = fTab.DataTable({
+            "responsive": true,
+            "data": data,
+            "createdRow": function (row, data, dataIndex) {
+                $(row).attr('data-index', data.id);
+            },
+            "columns": [
+                { "data": "id", responsivePriority: 1, },
+                { "data": "title", responsivePriority: 1, },
+                { "data": "id_category", responsivePriority: 1, },
+                { "data": "status", responsivePriority: 1, },
+                { "data": "creation_date", responsivePriority: 1, },
+                { "name": "actions" },
+            ],
+            "columnDefs": [{
+                targets: [5],
+                className: "text-center",
+                width: '10%',
+                orderable: false,
+                responsivePriority: 1,
+                render: function (data, type, row) {
+                    return `<i class="fa fa-trash" onClick="confirmRemove(deleteTag,${row.id})" style="cursor: pointer;"></i>`
+                }
+            }],
+        });
+    } else {
+        let ff = fTab.DataTable();
+        ff.clear().draw();
+        ff.rows.add(data);
+        ff.columns.adjust().draw();
+    }
+}
+
 let templateCategories = () => {
+    const thArr = ['id', 'tytuł', 'kategoria', 'status', 'dodano', ''];
     let temp = `
-        <div class="row" style="margin: 20px 5px 0px 5px;">
+        <div class="row" style="margin: 20px 5px 35px 5px;">
             ${pageHeader('Edycja postów')}
-
-            <!--
-            <div class="col-md-12">
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">First</th>P
-                        <th scope="col">Last</th>
-                        <th scope="col">Handle</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Larry</td>
-                        <td>the Bird</td>
-                        <td>@twitter</td>
-                    </tr>
-                </tbody>
-            </table>
+        <div class="col-md-12">
+            ${pageTable(thArr, tabID)}
         </div> 
-        -->
-
     </div>
     `;
     document.getElementById('root').innerHTML = temp;
+    getPosts();
 }
 
 templateCategories();
